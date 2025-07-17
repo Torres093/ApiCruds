a900b9e4e2f4be9c11c9bfaf246f7282f86f298f
Torres093 <marcosalejandrot583@gmail.com>
2025-07-17 15:01:21 -0600

    cualquier3

---
+++ src/main/java/practicaCrud/crud_API_MarcosTorres/Exceptions/ExcepcionUsuarioNoregistrado.java
@@ -0,0 +1,7 @@
+package practicaCrud.crud_API_MarcosTorres.Exceptions;
+
+public class ExcepcionUsuarioNoregistrado extends RuntimeException {
+  public ExcepcionUsuarioNoregistrado(String message) {
+    super(message);
+  }
+}
